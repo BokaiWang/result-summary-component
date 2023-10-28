2689e4a43c72700960c9f0539f538cd02b1fa5c5
BokaiWang <bk.wang1989@gmail.com>
2023-10-28 19:39:55 -0400

    Add the layout for desktop; Style the background the result card;

---
+++ src/components/Card.tsx
@@ -0,0 +1,10 @@
+const Card = () => {
+  return (
+    <div className="card">
+      Your Result 76 of 100 Great You scored higher than 65% of the people who
+      have taken these tests.
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/components/Summary.tsx
@@ -0,0 +1,10 @@
+const Summary = () => {
+  return (
+    <div>
+      Summary Reaction 80 / 100 Memory 92 / 100 Verbal 61 / 100 Visual 72 / 100
+      Continue
+    </div>
+  );
+};
+
+export default Summary;
